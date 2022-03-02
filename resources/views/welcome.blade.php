bff9baf4870253495f94c350d2af35f2626a604f
George Trindade Rodrigues <george.rodrigues@estudante.ifto.edu.br>
2022-03-02 19:32:55 -0300

    Atividade_Aula

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,5 @@
 <h1>CRUD-Laravel</h1>
-<a href="{{route('clientes.index')}}"><h2>1-Tabela de Clientes</h2></a><br/>
-<a href="{{route('veiculos.index')}}"><h2>2-Tabela de Veículos</h2></a><br/>
-<a href="{{route('servicos.index')}}"><h2>3-Tabela de Serviços</h2></a><br/>
+<h2>Tabelas</h2>
+<a href="{{route('clientes.index')}}"><h2>1-Clientes</h2></a><br/>
+<a href="{{route('veiculos.index')}}"><h2>2-Veículos</h2></a><br/>
+<a href="{{route('servicos.index')}}"><h2>3-Serviços</h2></a><br/>
